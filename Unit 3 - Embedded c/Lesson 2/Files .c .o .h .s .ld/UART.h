3630f0f79950d58a247bc6ced4b65da937405f93
Aly Mustafa <109925614+AlyMustafa@users.noreply.github.com>
2023-07-13 20:38:46 +0300

    Delete Unit 3 - Embedded c/Lesson 2 directory

--- Unit 3 - Embedded c/Lesson 2/Files .c .o .h .s .ld/UART.h
+++
@@ -1,5 +0,0 @@
-#ifndef _UART_H_
-#define _UART_H_
-void uart_send_string (unsigned char* P_to_string);
-
-#endif
--- Unit 3 - Embedded c/Lesson 2/Files .c .o .h .s .ld/app.c.c
+++
@@ -1,8 +0,0 @@
-#include "UART.h"
-
-unsigned char string_buffer[100] = "Aly-Mustafa-Enaya" ;
-
-void main(void){
-	uart_send_string (string_buffer);
-	
-}
