16c929e1cecfb5a11439b5b992c2718cf494a181
Aly Mustafa <109925614+AlyMustafa@users.noreply.github.com>
2023-12-03 11:22:07 +0200

    Add files via upload

---
+++ Unit 7 MCU Essential Peripherals/Lesson1 GPIO Part 1/Section/Task 1/Lab 1/Utils.h
@@ -0,0 +1,19 @@
+﻿/*
+ * Utils.h
+ *
+ * Created: 04/09/2023 05:21:58 م
+ *  Author: Metro
+ */ 
+
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#define SetBit(Reg,Bit) (Reg |=(1<<Bit)) //  (Reg = Reg|(1<<Bit))
+#define ResetBit(Reg,Bit) (Reg &=~(1<<Bit)) //  (Reg = Reg &~(1<<Bit))
+#define ToggleBit(Reg,Bit) (Reg ^=(1<<Bit)) //  (Reg = Reg^(1<<Bit))
+
+
+
+
+#endif /* UTILS_H_ */
